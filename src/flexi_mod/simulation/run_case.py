875ea6f498c71968e0945d17055f84a46cfbf8e4
manish-khanra <manishkhanra@gmail.com>
2026-07-29 09:36:46 +0200

    run case

--- src/flexi_mod/simulation/run_case.py
+++ src/flexi_mod/simulation/run_case.py
@@ -37,29 +37,29 @@ _GENERATED_STUDY_FAMILIES = (
 _GENERATED_YEARS = (
     # "2030",
     # "2035",
-    "2040",
+    # "2040",
     "2045",
 )
 
 _GENERATED_ROUTE_VARIANTS = (
     # "bf_bof_coal_external",
-    # "bf_bof_hybrid_hydrogen_natural_gas_electrolyser",  #this
+    "bf_bof_hybrid_hydrogen_natural_gas_electrolyser",  #this
     "bf_bof_hybrid_hydrogen_natural_gas_external",  #this
-    "bf_bof_hydrogen_electrolyser", #this
-    # "bf_bof_hydrogen_external",
-    # "bf_bof_natural_gas_external",
-    "dri_bof_coal_external", #this
-    "dri_bof_hybrid_hydrogen_natural_gas_electrolyser",  #this
-    # "dri_bof_hybrid_hydrogen_natural_gas_external",
-    "dri_bof_hydrogen_electrolyser",  #this
-    "dri_bof_hydrogen_external",  #this
-    "dri_bof_natural_gas_external",  #this
-    # "dri_eaf_coal_external",
-    "dri_eaf_hybrid_hydrogen_natural_gas_electrolyser",  #this
-    "dri_eaf_hybrid_hydrogen_natural_gas_external", #this
-    "dri_eaf_hydrogen_electrolyser",  #this
-    "dri_eaf_hydrogen_external", #this
-    "dri_eaf_natural_gas_external",  #this
+    # "bf_bof_hydrogen_electrolyser", #this
+    # # "bf_bof_hydrogen_external",
+    # # "bf_bof_natural_gas_external",
+    # "dri_bof_coal_external", #this
+    # "dri_bof_hybrid_hydrogen_natural_gas_electrolyser",  #this
+    # # "dri_bof_hybrid_hydrogen_natural_gas_external",
+    # "dri_bof_hydrogen_electrolyser",  #this
+    # "dri_bof_hydrogen_external",  #this
+    # "dri_bof_natural_gas_external",  #this
+    # # "dri_eaf_coal_external",
+    # "dri_eaf_hybrid_hydrogen_natural_gas_electrolyser",  #this
+    # "dri_eaf_hybrid_hydrogen_natural_gas_external", #this
+    # "dri_eaf_hydrogen_electrolyser",  #this
+    # "dri_eaf_hydrogen_external", #this
+    # "dri_eaf_natural_gas_external",  #this
 )
 GENERATED_EXAMPLE_NAMES = tuple(
     f"{family}_{year}_{variant}"
